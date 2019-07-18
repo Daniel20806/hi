c1e87fba4c82b46a82c327c6ea8acb5e2d4270df
Daniel20806 <angus20806@gmail.com>
2019-07-18 13:05:36 +0800

    second

--- 123.py
+++ 123.py
@@ -1,3 +1,10 @@
-name=input ('請輸入名字:')
-print ('嗨', name)
- 
+rain = input('請問今天有沒有下雨:')
+
+if rain == '有':
+	print('撐傘出門')
+	print('買一包洋芋片')
+	print('在家看電影')
+if rain == '沒有':
+	print('去遊樂園')
+	print('出門買食物')
+	print('帶太陽眼鏡')
